4d1c81cc74ebf064fda3c1437918ad788c5584ba
Elisabet S. Cillero <lizzyjadore@hotmail.com>
2016-09-26 19:43:18 +0200

    Creacion proyecto testing

---
+++ NapaMail/Repositories/IMensajeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using c11eindividual.Models;
+
+namespace c11eindividual.Repositories
+{
+    public interface IMensajeRepository
+    {
+        bool Borrar(int _id);
+        List<Mensaje> Entrada(int _id);
+        Mensaje Escribe(Mensaje _c);
+        Mensaje Lee(int _id);
+        Mensaje Modifica(Mensaje _c);
+        List<Mensaje> Papelera(int _id);
+        List<Mensaje> Salida(int _id);
+        List<Mensaje> Todos(int _id);
+    }
+}
---
+++ NapaMailTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using c11eindividual.Repositories;
+
+namespace NapaMailTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MensajeRepository sut = null;
+
+        [TestInitialize]
+        public void TestInitilize()
+        {
+            sut = new MensajeRepository();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var lista = sut.Todos(5);
+            Assert.AreEqual(2, lista.Count);
+        }
+    }
+}
